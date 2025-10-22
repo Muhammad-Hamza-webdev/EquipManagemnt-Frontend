abfd0d42ef0976fd14dbc6cb26037179148c8873
Muhammad Hamza <muhammadhamza.webdev@gmail.com>
2025-10-23 01:30:33 +0530

    testimionals

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,19 +2,3 @@
 import "./cdns.js";
 // Custom Select Element Functionalities
 import "./custom-select.js";
-
-// active tab code
-const tabs = document.querySelectorAll(".tab-btn");
-const contents = document.querySelectorAll(".tab-content");
-
-tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
-    // Remove active classes
-    tabs.forEach((t) => t.classList.remove("active"));
-    contents.forEach((c) => c.classList.remove("active-tab"));
-
-    // Add active to selected tab + content
-    tab.classList.add("active");
-    document.getElementById(tab.dataset.tab).classList.add("active-tab");
-  });
-});
---
+++ assets/js/testimonial.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const splide = new Splide(".splide", {
+    type: "loop",
+    perPage: 3,
+    perMove: 1,
+    gap: "2rem",
+    focus: "center",
+    pagination: false,
+    arrows: true,
+    autoplay: false,
+    drag: true,
+    breakpoints: {
+      1366: { perPage: 3 },
+      1200: { perPage: 3 },
+      1140: { perPage: 3 },
+      1024: { perPage: 3 },
+      880: { perPage: 1 },
+      768: { perPage: 1 },
+      550: { perPage: 1 },
+    },
+  });
+
+  splide.mount();
+});
