ce78d46be8456b30254f22edd298223ec5aa2dd1
Muhammad Hamza <muhammadhamza.webdev@gmail.com>
2025-10-27 21:29:38 +0530

    small issue
    A small issue needs to be fixed

--- assets/js/testimonial.js
+++ assets/js/testimonial.js
@@ -22,3 +22,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
   splide.mount();
 });
+document.addEventListener("DOMContentLoaded", function () {
+  const splides = new Splide(".carousel-1", {
+    type: "loop",
+    perPage: 4,
+    perMove: 1,
+    gap: "2rem",
+    focus: "center",
+    pagination: false,
+    arrows: true,
+    autoplay: false,
+    drag: true,
+    breakpoints: {
+      1366: { perPage: 3 },
+      1200: { perPage: 3 },
+      1140: { perPage: 3 },
+      1024: { perPage: 3 },
+      880: { perPage: 1 },
+      768: { perPage: 1 },
+      550: { perPage: 1 },
+    },
+  });
+
+  splides.mount();
+});
