c856e263806f5f7df19863b75b46c168119980dd
Muhammad Hamza <muhammadhamza.webdev@gmail.com>
2025-10-29 21:23:30 +0530

    New SwiperJS

--- assets/js/testimonial.js
+++ assets/js/testimonial.js
@@ -1,35 +1,36 @@
-var swiper = new Swiper(".mySwiper", {
+// First Swiper (mySwiper)
+var swiper1 = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 30,
   loop: true,
   autoplay: {
-    delay: 3000,
+    delay: 5000,
     disableOnInteraction: false,
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".mySwiper-next", // Unique selector
+    prevEl: ".mySwiper-prev", // Unique selector
   },
   breakpoints: {
     1366: {
       slidesPerView: 3,
-      spaceBetween: 20,
+      spaceBetween: 30,
     },
     1200: {
       slidesPerView: 3,
-      spaceBetween: 20,
+      spaceBetween: 30,
     },
     1140: {
       slidesPerView: 3,
-      spaceBetween: 20,
+      spaceBetween: 30,
     },
     1024: {
-      slidesPerView: 2,
-      spaceBetween: 20,
+      slidesPerView: 3,
+      spaceBetween: 30,
     },
     880: {
       slidesPerView: 1,
-      spaceBetween: 0,
+      spaceBetween: 20,
     },
     768: {
       slidesPerView: 1,
@@ -42,6 +43,55 @@ var swiper = new Swiper(".mySwiper", {
     0: {
       slidesPerView: 1,
       spaceBetween: 20,
+    },
+  },
+});
+
+// Second Swiper (categorySwiper with 4 slides)
+var swiper2 = new Swiper(".categorySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".categorySwiper-next", // Unique selector
+    prevEl: ".categorySwiper-prev", // Unique selector
+  },
+  breakpoints: {
+    1366: {
+      slidesPerView: 4,
+      spaceBetween: 0,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 0,
+    },
+    1140: {
+      slidesPerView: 4,
+      spaceBetween: 0,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 0,
+    },
+    880: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    550: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 0,
     },
   },
 });
